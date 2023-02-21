2f24b7eab002bceb962e70a64fe3dc61370e0c6a
赵海 <zh1179929172@163.com>
2023-02-21 16:16:15 +0800

    feat: 注释打包去除依赖的问题

--- vue.config.js
+++ vue.config.js
@@ -17,11 +17,11 @@ module.exports = {
         "@": path.resolve(__dirname, "src"),
       },
     },
-    externals: IS_PROD
-      ? {
-        vue: 'vue',
-        'element-plus': 'element-plus'
-      }
-      : {}
+    // externals: IS_PROD
+    //   ? {
+    //     vue: 'vue',
+    //     'element-plus': 'element-plus'
+    //   }
+    //   : {}
   }
 };
